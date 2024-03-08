122add7a0d78b37192e00630735808e7996f33a5
Tian Xiao <snoidetx@gmail.com>
2024-03-08 14:30:44 +0800

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,8 +1,7 @@
-from main import tableau
 import numpy as np
+from main import tableau
 
-
-AA = np.array([[1, 1, 1, -1]])
-bb = np.array([[3]]).T
-cc = np.array([[- 1, -1, -1, -1]]).T
-tableau(AA, bb, cc, (1,))
+A = np.array([[.7, .8, .9, 1, 0, 0], [.3, .2, .1, 0, 1, 0], [1, 1, -1, 0, 0, 1]])
+b = np.array([[60, 20, 0]]).T
+c = np.array([[-.7, .5, -.4, 0, 0, 0]]).T
+tableau(A, b, c, (4, 5, 6))
